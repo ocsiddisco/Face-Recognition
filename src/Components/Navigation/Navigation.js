ef72e7aea22a4bac1e1ff3367d9ced8b59f90c0c
Celine <celine.lancelle@gmail.com>
2023-04-24 12:14:20 +0200

    improve design

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React from 'react'
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-    if (isSignedIn) {
-        return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p
-                    onClick={() => onRouteChange('signout')}
-                    className="f3 link dim black underline pa3 pointer near-white"
-                >
-                    Sign Out
-                </p>
-            </nav>
-        )
-    } else {
-        return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p
-                    onClick={() => onRouteChange('signin')}
-                    className="f3 link dim black underline pa3 pointer near-white"
-                >
-                    Sign in
-                </p>
-                <p
-                    onClick={() => onRouteChange('register')}
-                    className="f3 link dim black underline pa3 pointer near-white"
-                >
-                    Register
-                </p>
-            </nav>
-        )
-    }
+  if (isSignedIn) {
+    return (
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+      >
+        <p
+          onClick={() => onRouteChange('signout')}
+          className="f3 link dim black underline pa3 pointer near-white"
+        >
+          Sign Out
+        </p>
+      </nav>
+    )
+  } else {
+    return (
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+      >
+        <p
+          onClick={() => onRouteChange('signin')}
+          className="f3 link dim black underline pa3 pointer near-white flex items-center"
+        >
+          Sign in
+        </p>
+        <p
+          onClick={() => onRouteChange('register')}
+          className="f3 link dim black underline pa3 pointer near-white flex items-center"
+        >
+          Register
+        </p>
+      </nav>
+    )
+  }
 }
 
 export default Navigation
